0954bf9326387fc902639f6aee3000489f4947f0
Kyra Boyle <kyra.boyle@students.plymouth.ac.uk>
2021-05-11 16:05:12 +0100

    yay
    yay

---
+++ Assets/AnimManager.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class AnimManager : MonoBehaviour
+{
+    public Animator[] animators;
+    public Vector3[] pageLocations;
+    public AudioClip[] audioClips;
+    public AudioClip clipRef;
+    public AudioSource source;
+    public GameObject vrCharacter;
+    public float touchTimer = 2f;
+    public int currentPage = 0;
+    public int lastPage = -1;
+    protected bool pressEnabled;
+    public VRTK_ControllerEvents controllerEvents;
+    protected VRTK_ControllerReference controllerReference;
+
+    private void Start()
+    {
+        pressEnabled = true;
+        vrCharacter.transform.position = pageLocations[currentPage];
+        //Camera.main.transform.position = pageLocations[currentPage];
+        source.clip = clipRef;
+        source.Play();
+    }
+
+
+    public void NextPage()
+    {
+        currentPage++;
+
+        currentPage = Mathf.Clamp(currentPage, 0, animators.Length - 1);
+
+        if (currentPage <= animators.Length -1)
+        {
+            clipRef = audioClips[currentPage];
+            if (animators[currentPage])
+            {
+                animators[currentPage].enabled = false;
+                animators[currentPage].enabled = true;
+                animators[currentPage].Play(0);
+            }
+            //Debug.Log("AHHH");
+            //Debug.Log(clipRef.name);
+            source.clip = clipRef;
+            source.Play();
+        }
+
+        vrCharacter.transform.position = pageLocations[currentPage];
+        //Camera.main.transform.position = pageLocations[currentPage];
+
+    }
+
+    public void LastPage()
+    {
+        currentPage--;
+
+        currentPage = Mathf.Clamp(currentPage, 0, animators.Length - 1);
+
+        if (currentPage <= animators.Length - 1)
+        {
+            clipRef = audioClips[currentPage];
+            if (animators[currentPage])
+            {
+                animators[currentPage].enabled = false;
+                animators[currentPage].enabled = true;
+                animators[currentPage].Play(0);
+            }
+            
+            source.clip = clipRef;
+            source.Play();
+        }
+
+        vrCharacter.transform.position = pageLocations[currentPage];
+        //Camera.main.transform.position = pageLocations[currentPage];
+
+    }
+
+
+    private void Update()
+    {
+        GameObject rightHand = VRTK_DeviceFinder.GetControllerRightHand(true);
+        controllerReference = VRTK_ControllerReference.GetControllerReference(rightHand);
+
+        if (ControllerForward())
+        {
+            NextPage();
+            pressEnabled = false;
+            StartCoroutine(EnableTouch());
+        }
+        else if (ControllerBackward())
+        {
+            LastPage();
+            pressEnabled = false;
+            StartCoroutine(EnableTouch());
+        }
+
+    }
+
+
+
+    protected virtual bool ControllerForward()
+    {
+        if (!VRTK_ControllerReference.IsValid(controllerReference))
+        {
+            return false;
+        }
+
+        return (pressEnabled && VRTK_SDK_Bridge.GetControllerButtonState(SDK_BaseController.ButtonTypes.Touchpad, SDK_BaseController.ButtonPressTypes.Press, controllerReference));
+    }
+
+    protected virtual bool ControllerBackward()
+    {
+        if (!VRTK_ControllerReference.IsValid(controllerReference))
+        {
+            return false;
+        }
+
+        return (pressEnabled && VRTK_SDK_Bridge.GetControllerButtonState(SDK_BaseController.ButtonTypes.Trigger, SDK_BaseController.ButtonPressTypes.Press, controllerReference));
+    }
+
+    IEnumerator EnableTouch()
+    {
+        yield return new WaitForSeconds(touchTimer);
+
+        pressEnabled = true;
+    }
+
+}
---
+++ Assets/NastyWizard_CelSobel/CelShadingSobel/Scripts/PostProcessing_Sobel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PostProcessing_Sobel : MonoBehaviour {
+
+    private Material sobelMat;
+    public Shader shader;
+
+    [Range(0, 10)]
+    public float SobelResolution = 1;
+
+    public Color outlineColor;
+
+
+    void Start () {
+        Camera.main.depthTextureMode = DepthTextureMode.Depth;
+        sobelMat = new Material(shader);
+    }
+	
+	void OnRenderImage (RenderTexture source, RenderTexture destination)
+    {
+        sobelMat.SetFloat("_ResX", Screen.width * SobelResolution);
+        sobelMat.SetFloat("_ResY", Screen.height * SobelResolution);
+        sobelMat.SetColor("_Outline", outlineColor);
+        Graphics.Blit(source, destination, sobelMat);
+    }
+}
